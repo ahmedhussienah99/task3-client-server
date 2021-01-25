e96bcac61e3407ec97ec0878c26a45f7976ef3dc
ahmedhussienah99 <61842483+ahmedhussienah99@users.noreply.github.com>
2021-01-25 18:39:23 +0200

    Create HMI_Template.c

---
+++ HMI_Template.c
@@ -0,0 +1,22 @@
+#include "Rte_HMI.h"
+void HMI_MainFunction (void)
+{
+	Std_ReturnType status;
+	MultiStateBtnType HeightBtnState;
+	MultiStateBtnType InclineBtnState;
+	MultiStateBtnType SlideBtnState;
+	uint8 Height;
+	uint8 Incline;
+	uint8 Slide;
+
+	/* Data Send Points */
+	status = Rte_Write_ppSeatCtrlBtns_HeightBtnState(HeightBtnState);
+	status = Rte_Write_ppSeatCtrlBtns_InclineBtnState(InclineBtnState);
+	status = Rte_Write_ppSeatCtrlBtns_SlideBtnState(SlideBtnState);
+	
+	/* Data Receive Points */
+	status = Rte_Read_rpSeatCtrlData_Height(&Height);
+	status = Rte_Read_rpSeatCtrlData_Incline(&Incline);
+	status = Rte_Read_rpSeatCtrlData_Slide(&Slide);
+	
+}
